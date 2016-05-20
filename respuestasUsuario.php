9077207a6d8cbe9a94644a09bd0367bd9846831e
aguskoll <aguskoll@gmail.com>
2016-05-20 11:12:49 -0300

    get oferta codigo anda

--- respuestasUsuario.php
+++ respuestasUsuario.php
@@ -13,16 +13,21 @@
  */
 require 'Database.php';
 
-  
+ //http://localhost:85/webServiceAndroid2/respuestasUsuario.php?funcion=getOfertas 
 
     
 //if ($_SERVER['REQUEST_METHOD'] == 'GET') 
 if (isset($_GET['funcion'])) {
     $funcion = $_GET['funcion'];
+    echo '<html> funcion es '.$funcion.' </html>';
 } 
+
 switch ($funcion) {
     case "getOfertas":
         getOfertas();
+        break;
+    case "getOfertaCodigo":
+       getOfertaCodigo();
         break;
 }
 
@@ -46,5 +51,39 @@ function getOfertas(){
         ));
     }
 }
+    /**
+    retorna una oferta parseada en json, cuyo codigo es $codigo
+     *     
+     *  */
+    function  getOfertaCodigo(){   
+        
+        if (isset($_GET['codigo'])) {
+             $codigo = $_GET['codigo'];
+        }
+        else{
+            echo '<html> no hay codigo </html>';
+        }
+       echo '<html>el codigo es '.$codigo.'</html>'; 
+    
+       $db= new Database();
+    $oferta= $db->getOfertaCodigo($codigo); 
+    
+    if ($oferta) 
+    {
+
+        $datos["estado"] = 1;
+        $datos["oferta"] = $oferta;
+
+        print json_encode($datos);
+       
+    }
+    else 
+        {
+            print json_encode(array(
+                "estado" => 2,
+                "mensaje" => "Ha ocurrido un error"
+            ));
+        }
+    }
 
 
